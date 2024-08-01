6e7329a9814cda1ae8e44f209c92ac320e69f578
Alef <csilva.alef@gmail.com>
2024-08-01 00:28:05 -0400

    build: configure i18n support for translate validation messages

---
+++ config/i18n.ts
@@ -0,0 +1,14 @@
+import app from '@adonisjs/core/services/app'
+import { defineConfig, formatters, loaders } from '@adonisjs/i18n'
+
+const i18nConfig = defineConfig({
+  defaultLocale: 'pt',
+  formatter: formatters.icu(),
+  loaders: [
+    loaders.fs({
+      location: app.languageFilesPath(),
+    }),
+  ],
+})
+
+export default i18nConfig
