a95577776229f64ed788259a7bc92ca70a962a3e
Alef <csilva.alef@gmail.com>
2024-08-01 12:14:50 -0400

    docs: configure swagger for farms

--- app/controllers/farm_controller.ts
+++ app/controllers/farm_controller.ts
@@ -7,24 +7,55 @@ import { storeFarmValidator, updateFarmValidator } from '#validators/farm_valida
 export default class FarmController {
   constructor(private readonly farmService: FarmService) {}
 
+  /**
+   * @index
+   * @operationId getFarms
+   * @paramPath farmerId - The id of farmer - @type(number) @required
+   * @description Returns array of farms
+   * @responseBody 200 - <Farm[]>
+   */
   async index({ params }: HttpContext) {
     return this.farmService.findAll(params.farmerId)
   }
 
+  /**
+   * @show
+   * @paramPath id - The id of farm - @type(number) @required
+   * @description Returns the farm with cultures
+   * @responseBody 200 - <Farm>.with(cultures)
+   */
   async show({ params }: HttpContext) {
     return this.farmService.findOne(params.id)
   }
 
+  /**
+   * @store
+   * @description Create a new farm
+   * @requestBody <CreateFarmDTO>
+   * @responseBody 200 - <Farm>
+   */
   async store({ request }: HttpContext) {
     const createFarmDto = await request.validateUsing(storeFarmValidator)
     return this.farmService.store(createFarmDto)
   }
 
+  /**
+   * @update
+   * @paramPath id - The id of farm - @type(number) @required
+   * @description Update the farm
+   * @requestBody <UpdateFarmDTO>
+   * @responseBody 200 - <Farm>
+   */
   async update({ params, request }: HttpContext) {
     const updateFarmDto = await request.validateUsing(updateFarmValidator)
     return this.farmService.update(params.id, updateFarmDto)
   }
 
+  /**
+   * @destroy
+   * @paramPath id - The id of farm - @type(number) @required
+   * @description Delete the farm
+   */
   async destroy({ params }: HttpContext) {
     return this.farmService.destroy(params.id)
   }
